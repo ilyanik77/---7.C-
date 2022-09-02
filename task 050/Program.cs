c66e0d8e240eedf8c714b111b20440575ba43fd4
ilyanik77 <ilyakhlamov77@gmail.com>
2022-09-02 21:10:42 +0300

    "Добавили решение"

---
+++ task 050/Program.cs
@@ -0,0 +1,51 @@
+﻿//  Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+//  и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+int [,] CreateArray (int n, int m)
+{
+    int [,] arr = new int [n, m];
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr [i, j] = new Random().Next(1, 100);
+        }
+    }
+    return arr;
+}
+
+void PrintArray (int [,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr [i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+    
+}
+
+void Elem (int [,] arr, int x, int y)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (x > arr.GetLength(0) || y > arr.GetLength(1))
+            Console.WriteLine("Такого элемента не существует");
+            
+        }
+    }
+    Console.Write(arr[x, y]); 
+}
+
+int [,] matrix = CreateArray(2, 4);
+PrintArray(matrix);
+Console.WriteLine();
+Elem(matrix, 1, 2);
+
+
+
+
